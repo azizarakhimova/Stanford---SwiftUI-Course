ca565e4f2547df6cc15fcee93dbf0dbe824342cc
azizarakhimova <148251453+azizarakhimova@users.noreply.github.com>
2024-06-20 07:50:22 +0200

    added model for view and started writing view model

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,23 +10,19 @@ import SwiftUI
 struct ContentView: View {
     let emojis: Array<String> = ["🧟","🧳","🥰","👻","🤓","🍚","🥗","📚","🛬","😇","👍"]  //Array<String> can be also written as [String]
     
-    @State var cardCount: Int = 4
     
     var body: some View {
-        VStack{
             ScrollView{
             cards
         }
-            Spacer()
-           cardCountAdjusters
-        }
+           
         .padding()
        
     }
     
     var cards: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
-            ForEach(0..<cardCount, id: \.self) {i in
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]){
+            ForEach(emojis.indices, id: \.self) {i in
                 CardView(content:emojis[i])
                     .aspectRatio(2/3, contentMode: .fit)
             }
@@ -34,34 +30,6 @@ struct ContentView: View {
         .foregroundColor(.orange)
     }
     
-    var cardCountAdjusters: some View{
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.title)
-    }
-    
-    // by in function is external parameter name and offset is internal parameter name
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-                cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-       return cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
-    }
 }
 
 #Preview {
---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Aziza Rahimova on 20/06/24.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGame{
+    var model: MemoryGame<String>
+}
